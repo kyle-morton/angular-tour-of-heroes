390b63a8c6c4e5a5da4e722f27085e160e82be69
Kyle Morton <kylemortondevelopment@gmail.com>
2018-05-24 13:32:57 -0500

    created mock list of heroes and added *ngFor to hero component

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # angular-tour-of-heroes
 
 https://angular.io/tutorial/ 
+
+## Current Tutorial Spot-
+https://angular.io/tutorial/toh-pt2#style-the-heroes
+
+### Module 
+- container of components
+- injects dependencies into components it imports
+
+### Component
+- template, ts, styling, test
+- can reference other components template in __same__ module
+
---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Jake the Snake Roberts' },
+    { id: 12, name: 'Macho Man Randy Savage' },
+    { id: 13, name: 'Rick Rude' },
+    { id: 14, name: 'Ric Flair' },
+    { id: 15, name: 'Vader' },
+    { id: 16, name: 'Sting' },
+    { id: 17, name: 'Hulk Hogan' },
+    { id: 18, name: 'Stone Cold Steve Austin' },
+    { id: 19, name: 'Arn Anderson' },
+    { id: 20, name: 'The Undertaker' }
+]
