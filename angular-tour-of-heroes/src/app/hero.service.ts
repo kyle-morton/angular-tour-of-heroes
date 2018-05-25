961b47712999d570b5aef92803fe8a6592a35788
Kyle Morton <kylemortondevelopment@gmail.com>
2018-05-25 13:43:36 -0500

    added http calls to hero service

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,21 +1,56 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  getHeroes(): Observable<Hero[]> { //returns observable array that can be subscribed to to get changes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
-  }
-  getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`); // ` allows for template literals (string interpolation) in JS
-    return of(HEROES.find(hero => hero.id === id));
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    this.log('getting heroes');
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(heroes => this.log(`fetched heroes`)),
+      catchError(this.handleError('getHeroes', []))
+    );
   }
 
-  constructor(private messageService: MessageService) { }
+  /** GET hero by id. Will 404 if id not found */
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+   
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+   
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+   
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) { }
 }
